14ac54570f0d43ee9ef347d9e55abb2e469a4aef
Design And Creation <romain.doyen97@gmail.com>
2025-01-03 10:56:01 +0400

    refactor(home.dart): Add a custom navigation bar

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -23,6 +23,60 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
+      bottomNavigationBar: NavigationBar(),
+    );
+  }
+
+  Widget NavigationBar() {
+    return Container(
+      child: Container(
+        decoration: BoxDecoration(
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                spreadRadius: 5,
+                blurRadius: 10),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30),
+            topRight: Radius.circular(30),
+          ),
+          child: BottomNavigationBar(
+            selectedItemColor: const Color(0xFF5F67EA),
+            selectedFontSize: 12,
+            unselectedFontSize: 12,
+            unselectedItemColor: Colors.grey.withOpacity(0.7),
+            type: BottomNavigationBarType.fixed,
+            items: [
+              const BottomNavigationBarItem(
+                label: 'Home',
+                icon: Icon(
+                  Icons.home_rounded,
+                  size: 50,
+                ),
+              ),
+              BottomNavigationBarItem(
+                label: "Contact",
+                icon: Container(
+                  margin: const EdgeInsets.all(5),
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Icon(
+                    Icons.contacts_rounded,
+                    size: 30,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
