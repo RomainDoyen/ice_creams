e956106e9ce84069c3e4e974da074b9afcfd5851
Design And Creation <romain.doyen97@gmail.com>
2025-01-03 16:29:42 +0400

    feat(home/widgets/category.dart): Removal of the fixed height to create a dynamic container based on content

--- lib/pages/home/widgets/category.dart
+++ lib/pages/home/widgets/category.dart
@@ -31,7 +31,6 @@ class CategorySection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 1000,
       decoration: const BoxDecoration(
         color: Colors.white,
         borderRadius: BorderRadius.only(
