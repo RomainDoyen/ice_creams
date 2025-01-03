264820e037895bd67fecfa2c00b7605134d810b4
Design And Creation <romain.doyen97@gmail.com>
2025-01-03 09:45:46 +0400

    refactor(assets/images): Create an assets folder containing images and fonts

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -5,8 +5,22 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Color(0xFFC8B7F7),
+    return Scaffold(
+      backgroundColor: const Color(0xFFC8B7F7),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Transform(
+              transform: Matrix4.identity()..rotateZ(20),
+              origin: const Offset(150, 50),
+              child: Image.asset(
+                'assets/images/ice_cream.png',
+                width: 200
+              )
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
