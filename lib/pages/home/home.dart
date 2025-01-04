ddbe08b9fd24aa249139a9f3cf6643e7f16006e1
Design And Creation <romain.doyen97@gmail.com>
2025-01-04 17:47:00 +0400

    refactor(pages/home/home.dart): Creation of a contact page and integration of a navigation logic between the home page and the contact page

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -2,51 +2,65 @@ import 'package:flutter/material.dart';
 import 'package:ice_creams/pages/home/widgets/category.dart';
 import 'package:ice_creams/pages/home/widgets/header.dart';
 import 'package:ice_creams/pages/home/widgets/search.dart';
+import 'package:ice_creams/pages/contact/contact.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    SingleChildScrollView(
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.topCenter,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 50),
+              child: Image.asset(
+                'assets/images/ice_cream.png',
+                width: 150,
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              const HeaderSection(),
+              const SearchSection(),
+              CategorySection(),
+            ],
+          )
+        ],
+      ),
+    ),
+    const ContactPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFC8B7F7),
-      body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            Align(
-              alignment: Alignment.topCenter,
-              child: Padding(
-                padding: const EdgeInsets.only(top: 50),
-                child: Image.asset(
-                  'assets/images/ice_cream.png',
-                  width: 150,
-                ),
-              ),
-            ),
-            Column(
-              children: [
-                const HeaderSection(),
-                const SearchSection(),
-                CategorySection(),
-              ],
-            )
-          ],
-        ),
-      ),
-      bottomNavigationBar: NavigationBar(),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: _buildNavigationBar(),
     );
   }
 
-  Widget NavigationBar() {
+  Widget _buildNavigationBar() {
     return Container(
       color: Colors.white,
       child: Container(
         decoration: BoxDecoration(
           boxShadow: [
             BoxShadow(
-                color: Colors.grey.withOpacity(0.2),
-                spreadRadius: 5,
-                blurRadius: 10),
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 5,
+              blurRadius: 10,
+            ),
           ],
         ),
         child: ClipRRect(
@@ -55,6 +69,12 @@ class HomePage extends StatelessWidget {
             topRight: Radius.circular(30),
           ),
           child: BottomNavigationBar(
+            currentIndex: _currentIndex,
+            onTap: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
             selectedItemColor: const Color(0xFF5F67EA),
             selectedFontSize: 12,
             unselectedFontSize: 12,
@@ -73,14 +93,9 @@ class HomePage extends StatelessWidget {
                 icon: Container(
                   margin: const EdgeInsets.all(5),
                   padding: const EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                    color: Colors.grey.withOpacity(0.2),
-                    borderRadius: BorderRadius.circular(10),
-                  ),
                   child: const Icon(
                     Icons.contacts_rounded,
-                    size: 30,
-                    color: Colors.grey,
+                    size: 40,
                   ),
                 ),
               ),
