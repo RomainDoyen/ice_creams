34063259c740c2abee875a34c5f1d50c07f3e1a8
Design And Creation <romain.doyen97@gmail.com>
2025-01-05 16:39:08 +0400

    refactor(pages/contact/widgets/maps.dart): Redesign of popup elements

--- lib/pages/contact/widgets/maps.dart
+++ lib/pages/contact/widgets/maps.dart
@@ -12,21 +12,21 @@ class MapsBaseWidget extends StatefulWidget {
 class _MapsBaseWidgetState extends State<MapsBaseWidget> {
   final List<Map<String, dynamic>> shops = [
     {
-      "name": "IceCreams Sainte-Anne",
+      "name": "IceCreams - Sainte-Anne",
       "address": "305 Route Nationale 2 (Sainte-Anne), 97437 Saint-Benoît, France",
       "phone": "+262 262 123 456",
       "latitude": -21.096574918606812,
       "longitude": 55.75469888238227,
     },
     {
-      "name": "IceCreams Saint-Louis",
+      "name": "IceCreams - Saint-Louis",
       "address": "38 Rue Sainte Therese, 97450 Saint-Louis, France",
       "phone": "+262 262 654 321",
       "latitude": -21.283634612247532,
       "longitude": 55.41052402822798,
     },
     {
-      "name": "IceCreams Saint-Denis",
+      "name": "IceCreams - Saint-Denis",
       "address": "8 Allée Bellavista, 97400 Saint-Denis, France",
       "phone": "+262 262 789 123",
       "latitude": -20.904876553075294,
@@ -58,18 +58,65 @@ class _MapsBaseWidgetState extends State<MapsBaseWidget> {
                     showDialog(
                       context: context,
                       builder: (context) => AlertDialog(
-                        title: Text(shop["name"]),
+                        title: Text(
+                          shop["name"],
+                          style: const TextStyle(
+                            fontSize: 20,
+                            color: Colors.deepPurple,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
                         content: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           mainAxisSize: MainAxisSize.min,
                           children: [
-                            Text("Address: ${shop["address"]}"),
+                            RichText(
+                              text: TextSpan(
+                                children: [
+                                  const TextSpan(
+                                    text: "Address: ",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: shop["address"],
+                                    style: const TextStyle(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
                             const SizedBox(height: 10),
-                            Text("Phone: ${shop["phone"]}"),
+                            RichText(
+                              text: TextSpan(
+                                children: [
+                                  const TextSpan(
+                                    text: "Phone: ",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: shop["phone"],
+                                    style: const TextStyle(
+                                      color: Colors.grey,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
                           ],
                         ),
                         actions: [
                           TextButton(
+                            style: ButtonStyle(
+                              backgroundColor: WidgetStateProperty.all(Colors.deepPurple),
+                              foregroundColor: WidgetStateProperty.all(Colors.white),
+                            ),
                             onPressed: () => Navigator.pop(context),
                             child: const Text("Close"),
                           ),
@@ -80,7 +127,7 @@ class _MapsBaseWidgetState extends State<MapsBaseWidget> {
                   child: Image.asset(
                     'assets/images/marker_ice_cream.png',
                     height: 40,
-                    width: 40, 
+                    width: 40,
                   ),
                 ),
               );
