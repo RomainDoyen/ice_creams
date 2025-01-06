288b76847b082679e638c6dc137c69c73d299b35
Design And Creation <romain.doyen97@gmail.com>
2025-01-06 10:53:38 +0400

    refactor(home/widgets/newest.dart): Add logic to make the element clickable and direct it to the page detail

--- lib/pages/home/widgets/newest.dart
+++ lib/pages/home/widgets/newest.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ice_creams/models/icecream.dart';
+import 'package:ice_creams/pages/detail/detail.dart';
 
 class NewestIceCream extends StatelessWidget {
   NewestIceCream({super.key});
@@ -11,84 +12,97 @@ class NewestIceCream extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(25),
       child: Column(
-        children: icecreams.map((icecream) => Container(
-          padding: const EdgeInsets.all(15),
-          margin: const EdgeInsets.only(bottom: 20),
-          decoration: BoxDecoration(
-            color: const Color(0xFFE3EEFF),
-            borderRadius: BorderRadius.circular(15),
-          ),
-          child: Row(
-            children: [
-              Container(
-                width: 60,
-                height: 60,
-                decoration: BoxDecoration(
-                  color: Colors.grey.withOpacity(0.1), 
-                  borderRadius: BorderRadius.circular(10), 
+        children: icecreams.map((icecream) {
+          return GestureDetector(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => DetailPage(iceCream: icecream),
                 ),
-                child: Padding(
-                  padding: const EdgeInsets.all(10), 
-                  child: Image.asset(icecream.icon),
-                ),
+              );
+            },
+            child: Container(
+              padding: const EdgeInsets.all(15),
+              margin: const EdgeInsets.only(bottom: 20),
+              decoration: BoxDecoration(
+                color: const Color(0xFFE3EEFF),
+                borderRadius: BorderRadius.circular(15),
               ),
-              const SizedBox(width: 10),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      icecream.name,
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
+              child: Row(
+                children: [
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(10),
                     ),
-                    const SizedBox(height: 5),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Image.asset(icecream.icon),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+                        Text(
+                          icecream.name,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text(
-                              icecream.type,
-                              style: const TextStyle(
-                                color: Colors.grey,
-                                fontSize: 12,
-                              ),
-                            ),
-                            const SizedBox(height: 2),
-                            const Row(
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
                               children: [
-                                Icon(Icons.star, color: Colors.yellow, size: 15),
+                                Text(
+                                  icecream.type,
+                                  style: const TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                                const SizedBox(height: 2),
+                                const Row(
+                                  children: [
+                                    Icon(Icons.star,
+                                        color: Colors.yellow, size: 15),
+                                  ],
+                                )
                               ],
+                            ),
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 5),
+                              decoration: BoxDecoration(
+                                color: const Color(0xFFBB71B4),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Text(
+                                '${icecream.price} \$',
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12,
+                                ),
+                              ),
                             )
                           ],
                         ),
-                        Container(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 10, vertical: 5),
-                          decoration: BoxDecoration(
-                            color: const Color(0xFFBB71B4),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Text(
-                            '${icecream.price} \$',
-                            style: const TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                            ),
-                          ),
-                        )
                       ],
                     ),
-                  ],
-                ),
-              )
-            ],
-          ),
-        )).toList(),
+                  )
+                ],
+              ),
+            ),
+          );
+        }).toList(),
       ),
     );
   }
